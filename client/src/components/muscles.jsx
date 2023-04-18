978fb380766d0f401b976e8418dfd9855e1bd57a
MariolaH <mariola.hullings@student.awesomeinc.org>
2023-04-18 18:57:08 +0000

    filter exercises

--- client/src/components/muscles.jsx
+++ client/src/components/muscles.jsx
@@ -2,51 +2,42 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import Nav from "./Nav";
 
-
 const BASE_URL =
   "https://8000-mariolah-backendflex-x1j83bg0m41.ws-us94.gitpod.io/api/";
 
 function Muscles() {
+  const [muscle, setMuscle] = useState([]);
+  const [exercise, setExercise] = useState([]);
+  const [selectMuscle, setSelectMuscle] = useState("");
 
-    const [muscle, setMuscle] = useState([]);
-    const [exercise, setExercise] = useState([]);
-    const [selectMuscle, setSelectMuscle] = useState("");
-
-    useEffect(() => {
-      const getMuscle = async () => {
-        let config = {
-          url: "/musclegroup/",
-          baseURL: BASE_URL,
-          method: "get",
-        };
-        let response = await axios.request(config);
-    
-        setMuscle(response.data);
-       
-      }
-      getMuscle();
-    }, []);
-
-
-      useEffect(() => {
-        const getExercise = async () => {
-          if (selectMuscle !== "") {
-          let config = {
-            url: "/exerciselist/",
-            baseURL: BASE_URL,
-            method: "get",
-          };
-          let response = await axios.request(config);
-          setExercise(response.data);
-          }
-        };
-        getExercise();
-      }, [selectMuscle]);
-
-      const handleMuscleGroupClick = (muscle) => {
-        setSelectMuscle(muscle);
+  useEffect(() => {
+    const getMuscle = async () => {
+      let config = {
+        url: "/musclegroup/",
+        baseURL: BASE_URL,
+        method: "get",
       };
-   
+      let response = await axios.request(config);
+
+      setMuscle(response.data);
+    };
+    getMuscle();
+
+    const getExercise = async () => {
+      let config = {
+        url: "/exerciselist/",
+        baseURL: BASE_URL,
+        method: "get",
+      };
+      let response = await axios.request(config);
+      setExercise(response.data);
+    };
+    getExercise();
+  }, []);
+
+  const handleMuscleGroupClick = (muscle) => {
+    setSelectMuscle(muscle);
+  };
 
   return (
     <>
@@ -61,11 +52,12 @@ function Muscles() {
         </button>
       ))}
 
-     
-      {exercise.map((exercise) => (
-        <p> {exercise.name} </p>
-      ))}
-    </>
+      {exercise
+        .filter((e) => e.muscles.some((m) => m.name === selectMuscle))
+        .map((exercise) => (
+          <p key={exercise.id}> {exercise.name} </p>
+        ))}
+ç    </>
   );
 }
 
