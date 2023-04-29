d57d5857b1133d56f5821a1ab5743c9c34a7da78
MariolaH <mariola.hullings@student.awesomeinc.org>
2023-04-29 15:54:24 +0000

    added api

--- client/src/services/auth.constants.js
+++ client/src/services/auth.constants.js
@@ -1,5 +1,6 @@
 export const API_URL =
-  "https://8000-mariolah-backendflex-x1j83bg0m41.ws-us96.gitpod.io/api/";
+  // "https://8000-mariolah-backendflex-x1j83bg0m41.ws-us96.gitpod.io/api/";
+  "https://ainc-flex-backend.ue.r.appspot.com/api/";
 export const LOGIN_ENDPOINT = "user/login/";
 export const REGISTER_ENDPOINT = "user/signup/";
 export const REFRESH_ENDPOINT = "token/refresh/";
