9194992bf8d1e960a6561a1408a3a658db8d5d64
MariolaH <mariola.hullings@student.awesomeinc.org>
2023-04-29 13:51:13 +0000

    update

--- client/src/components/muscles.jsx
+++ client/src/components/muscles.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useRef } from "react";
+import { useEffect, useState} from "react";
 import { Link } from "react-router-dom";
 import request from "../services/api.request";
 import Nav from "./Nav";
